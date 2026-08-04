22f801f42bd5e07aa8bda01ab522e77252859ade
balaya <balaya@balayas-MacBook-Air.local>
2026-08-04 12:51:36 +0530

    removed all changes

--- my-first-container/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projects/',views.ProjectViewSet),
-    path('task/<int:pk>/',views.Listviewset),
-    path('delete/<int:pk>/',views.delete_task)
-
-]
